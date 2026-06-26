ab8565b54863539ef969baf9d9be852616c7750e
mhubsources <mahdidarzi1024@gmail.com>
2026-06-26 19:46:35 +0330

    chore: TaskService.java has been updated

--- src/main/java/com/example/taskmanagement/service/TaskService.java
+++ src/main/java/com/example/taskmanagement/service/TaskService.java
@@ -1,10 +1,34 @@
 package com.example.taskmanagement.service;
 
 
+import com.example.taskmanagement.model.Task;
+import com.example.taskmanagement.repository.TaskRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 
 @Service
 public class TaskService {
+    private final TaskRepository taskRepository;
 
+    public TaskService(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+    public Task createTask(Task task) {
+        return taskRepository.save(task);
+    }
+
+    public List<Task> getAllTasks() {
+        return taskRepository.findAll();
+    }
+
+    public Task getTask(Long id) {
+        return taskRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Task not found"));
+    }
+
+    public List<Task> getTasksByPriority(String priority) {
+        return taskRepository.findByPriority(priority);
+    }
 }
